1b3a512a4c56444ac55825052774cd06161a8e2c
Purrplexe <piow375@gmail.com>
2025-12-01 12:32:05 +0100

    added text

--- Assets/Scripts/Python/main.py
+++ Assets/Scripts/Python/main.py
@@ -25,9 +25,9 @@ connector.start_listening(
 )
 
 print("connected")
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(1)
 #load our trained model
-model = YOLO(r"C:\Users\piow3\Documents\GitHub\Project-3\Assets\Scripts\Python\best.pt")
+model = YOLO(r"C:\Projects\Medialogi\Project-3\Assets\Scripts\Python\best.pt")
 while(True):
     # Create data structure
     data = { "Points": [] }
@@ -49,7 +49,7 @@ while(True):
                           (int(x1), int(y1)), 
                           (int(x2), int(y2)), 
                           (0, 255, 0), 2)
-                cv2.putText(frame, class_label, 
+                cv2.putText(frame, class_label + f" {box.conf[0]:.2f}", 
                         (int(x1), int(y1) - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 
                         0.7, (0, 255, 0), 2)
