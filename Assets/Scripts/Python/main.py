34e7ae55be5183024682acacdfa253c7f2faee30
Purrplexe <piow375@gmail.com>
2025-12-15 01:10:32 +0100

    fixed orientation
    flipped y axis before sending to unity.... took way too long to fix, also added a square to represent camera area

--- Assets/Scripts/Python/main.py
+++ Assets/Scripts/Python/main.py
@@ -40,10 +40,13 @@ while(True):
             if len(boxes) == 0:
                 continue
             for box in boxes:
-                #if box is a 2x6 (https://github.com/ultralytics/ultralytics/issues/2868)
+                # (https://github.com/ultralytics/ultralytics/issues/2868)
                 class_label = result.names[int(box.cls[0])]
                 # extract box coordinates
                 x1, y1, x2, y2 = box.xyxy[0]
+                #get center point
+                cx = (x1 + x2) / 2
+                cy = (y1 + y2) / 2
                 # Draw bounding box on frame
                 cv2.rectangle(frame, 
                           (int(x1), int(y1)), 
@@ -53,16 +56,22 @@ while(True):
                         (int(x1), int(y1) - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 
                         0.7, (0, 255, 0), 2)
-                #get center point
-                cx = (x1 + x2) / 2
-                cy = (y1 + y2) / 2
+                
+
+                cv2.circle(frame, 
+                           (int(cx),int(cy))
+                           ,1,(0, 0, 255),1)
+                #invert y axis https://stackoverflow.com/questions/39953263/get-video-dimension-in-python-opencv
+                cy = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT) -cy
                 #add point to data structure
+                
                 isHorizontal = bool((abs(x1-x2) < abs(y1-y2) ))
                 length = abs(x1-x2) if bool((abs(x1-x2) <  abs(y1-y2))) else abs(y1-y2)
                 data["Points"].append({ "brickType": str(class_label),"x": float(cx), "y": float(cy), "isHorizontal": bool(isHorizontal), "length": float(length)})
                 # send data to Unity
-            connector.send("box_coordinates", data)
-            data = { "Points": [] }
+        connector.send("box_coordinates", data)
+        data = { "Points": [] }
     cv2.imshow("Camera with Bounding Boxes", frame)
-    cv2.waitKey(1)
+    cv2.waitKey(5000)
+
             
